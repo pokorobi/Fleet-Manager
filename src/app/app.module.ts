5efe416511849ae61807d682f8a3928061e343ab
Poko Robert <pokorobi@gmail.com>
2020-04-28 17:45:05 +0200

    Step3: data and button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './part/menu/menu.component';
 import { DataTableComponent } from './part/data-table/data-table.component';
+import { DataRowComponent } from './part/data-row/data-row.component';
+import { DataCellComponent } from './part/data-cell/data-cell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DataTableComponent
+    DataTableComponent,
+    DataRowComponent,
+    DataCellComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/part/data-cell/data-cell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-cell',
+  templateUrl: './data-cell.component.html',
+  styleUrls: ['./data-cell.component.css']
+})
+export class DataCellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
